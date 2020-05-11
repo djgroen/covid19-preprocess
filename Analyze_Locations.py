817b1b85bc6ba20810e541313a99674c82bcd0e7
imahmood786 <imran.mahmood@seecs.edu.pk>
2020-05-11 13:42:29 +0100

    Updated location Graph for Hillingdon after modifying the OSM file

--- Analyze_Locations.py
+++ Analyze_Locations.py
@@ -4,11 +4,16 @@ import  plotly as py
 import pandas as pd
 import sys
 
+def GetColor(type):
+    return "red"
 
-df = pd.read_csv('./hillingdon-out/hillingdon_buildings.csv', header=None, delimiter=',')
+df = pd.read_csv('./brent-out/brent_buildings.csv', header=None, delimiter=',')
 df.columns = ['type', 'x', 'y', 'area']
 groups = df.groupby('type').count()
-
+type = ['hospital', 'house', 'office', 'park', 'leisure', 'school', 'supermarket', 'shopping']
+df['color'] = 0
+for index, row in df.iterrows():
+    df['color'][index] = type.index(row['type'])
 # df = df[df['building']!='yes']
 # df.size
 
@@ -16,9 +21,9 @@ groups = df.groupby('type').count()
 #         print(col)
 
 fig = px.scatter_mapbox(df, lat="y", lon="x",
-                        color_discrete_sequence=["crimson"],
                         hover_name="type",
+                        color = 'color',
+                        color_continuous_scale=["darkgreen", "crimson", "orange", "lightgreen", "gold", "purple", "blue", "blue"],
                         zoom=8,  height=800)
 fig.update_layout(mapbox_style="open-street-map")
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 py.offline.plot(fig, filename='name.html')
