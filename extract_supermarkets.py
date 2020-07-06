10b6f2a89eee39c2f6852437f2a2266b7a52dda5
imahmood786 <imran.mahmood@seecs.edu.pk>
2020-07-06 09:07:04 +0500

    Updated location Graph for Hillingdon after modifying the OSM file

--- extract_supermarkets.py
+++ extract_supermarkets.py
@@ -2,33 +2,53 @@ import sys
 import xml.etree.ElementTree as ET
 from parse_osm import *
 
-def extract_supermarkets(tree):
+def extract_supermarkets(tree, flag):
   for c1 in root:
     if c1.tag == "way":
       for c2 in c1:
-          if get_tag(c2, "landuse") == "retail" or get_tag(c2, "amenity") == "supermarket" or get_tag(c2, "other_tags") == "supermarket":
-          p = get_polygon_from_way(c1, node_list)
-          if p:
-            #print(get_nodes(c1))
-            #print(c2.tag, c2.attrib, p.centroid, calc_geom_area(p))
-            print("supermarket,{},{},{}.{}".format(p.centroid.x, p.centroid.y, int(calc_geom_area(p)), get_tag(c2,"other_tags")))
-            break
-          else:
-            p = get_polygon_from_way(c1, node_list)
-            if p:
-              #print(get_nodes(c1))
-              #print(c2.tag, c2.attrib, p.centroid, calc_geom_area(p))
-              print("shopping,{},{},{}".format(p.centroid.x, p.centroid.y, int(calc_geom_area(p))))
-              break
+        if flag==True:
+          if get_tag(c2, "shop") or get_tag(c2, "landuse") == "retail"  or get_tag(c2, "building") == "retail":
+            if get_tag(c2, "amenity") == "supermarket" or get_tag(c2, "other_tags") == "supermarket":
+              p = get_polygon_from_way(c1, node_list)
+              if p:
+                #print(get_nodes(c1))
+                #print(c2.tag, c2.attrib, p.centroid, calc_geom_area(p))
+                print("supermarket,{},{},{}".format(p.centroid.x, p.centroid.y, int(calc_geom_area(p))))
+                break
+            else:
+              p = get_polygon_from_way(c1, node_list)
+              if p:
+                #print(get_nodes(c1))
+                #print(c2.tag, c2.attrib, p.centroid, calc_geom_area(p))
+                print("shopping,{},{},{}".format(p.centroid.x, p.centroid.y, int(calc_geom_area(p))))
+                break
+        else:
+          if get_tag(c2, "shop"):
+            if get_tag(c2, "shop") == "supermarket" or get_tag(c2, "amenity") == "supermarket" or get_tag(c2, "other_tags") == "supermarket":
+              p = get_polygon_from_way(c1, node_list)
+              if p:
+                #print(get_nodes(c1))
+                #print(c2.tag, c2.attrib, p.centroid, calc_geom_area(p))
+                print("supermarket,{},{},{}".format(p.centroid.x, p.centroid.y, int(calc_geom_area(p))))
+                break
+            else:
+              p = get_polygon_from_way(c1, node_list)
+              if p:
+                #print(get_nodes(c1))
+                #print(c2.tag, c2.attrib, p.centroid, calc_geom_area(p))
+                print("shopping,{},{},{}".format(p.centroid.x, p.centroid.y, int(calc_geom_area(p))))
+                break
 
   print("Debug: list of leisure types in osm file:", leisure_types, file=sys.stderr)
+
 
 if __name__ == "__main__":
   tree = ET.parse(sys.argv[1])
 
   root = tree.getroot()
-
   leisure_types = {}
   node_list = build_node_list(root)
 
-  extract_supermarkets(tree)
+  # use flag = true to execute special code block for landuse and buildings = retail
+  flag = True
+  extract_supermarkets(tree, flag)
